ed57ecc4092cdcb835cb0dd7250012c4aa165ab5
MadhuriYadav2521 <madhuri.y.0932@gmail.com>
2023-08-06 22:35:17 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -211,3 +211,56 @@ export const adminLogin = async (req,res) => {
         return res.send(err)
     }
 }
+
+
+
+// router.post('/login',  function (req, res, next) {
+//     if (req.body.username && req.body.password)
+//     {
+//       User.authenticate(req.body.username, req.body.password, function (error, user)
+//       {
+//         if (error || !user)
+//         {
+//           var err = new Error('Wrong username or password.');
+//           err.status = 401;
+//           return res.redirect('/login');
+//         }
+//         else
+//         {
+//           req.session.userId = user._id;
+//           if(user.roles=='admin')
+//           {
+//           return res.redirect(`/admin/${req.body.username}`);
+//           }else if(user.roles=='teacher')
+//           {
+//             console.log('teacher login');
+//             return res.redirect('/teacher');
+//           }else
+//           {
+//             console.log('Time Table Creator login');
+//             return res.redirect('/creator');
+//           }
+
+//         }
+//       });
+//     }else
+//     {
+//       var err = new Error('All fields required.');
+//       err.status = 400;
+//       return next(err);
+//     }
+//   });
+
+//   router.get('/login',function(req,res){
+//     res.render('login', { });
+//   });
+
+//   //Login Logic ends
+
+//   //Get page after login
+//   router.get('/admin/:id', mid, function(req, res) { // change your route like this
+
+//       res.render('admin_dashboard',{});
+
+
+// });
